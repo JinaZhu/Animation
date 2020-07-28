f435b30602c9a6aaab843b34b5a5bc656d695366
Jina Zhu <jinazhu87@gmail.com>
2020-07-28 10:20:12 -0700

    add comments to app.js

--- text/app.js
+++ text/app.js
@@ -1,25 +1,34 @@
+// grad the element
 const text = document.querySelector(".fancy");
+// get the text
 const strText = text.textContent;
+// turn text into an array
 const splitText = strText.split("");
+// set element text to be nothing
 text.textContent = "";
 
+// add a span to each letter
 for (let i = 0; i < splitText.length; i++) {
   text.innerHTML += "<span>" + splitText[i] + "</span>";
 }
 
 let char = 0;
-let timer = setInterval(onTick, 50);
+// every two .2 seconds, run  onTick function
+let timer = setInterval(onTick, 200);
 
+// animation the letter at the char index
 function onTick() {
   const span = text.querySelectorAll("span")[char];
   span.classList.add("fade");
   char++;
+  // when the char reach the last letter, call the complete function
   if (char === splitText.length) {
     complete();
     return;
   }
 }
 
+// stop the animation
 function complete() {
   clearInterval(timer);
   timer = null;
