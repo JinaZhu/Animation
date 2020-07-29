e16c1138292ca26353ae55a495190405f414dae0
Jina Zhu <jinazhu87@gmail.com>
2020-07-29 10:49:52 -0700

    add comments to app js for cursor

--- cursor/app.js
+++ cursor/app.js
@@ -3,11 +3,13 @@ let navLinks = document.querySelectorAll(".nav-links li");
 
 window.addEventListener("mousemove", cursor);
 
+// add top and left positioning to the cursor bubble
 function cursor(e) {
   mouseCursor.style.top = e.pageY + "px";
   mouseCursor.style.left = e.pageX + "px";
 }
 
+// when mouseover nav links, the bubble will grow and the link color will change to white
 navLinks.forEach((link) => {
   link.addEventListener("mouseover", () => {
     mouseCursor.classList.add("link-grow");
